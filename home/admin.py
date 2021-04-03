0e92f5724dc86242fdca1b433ec0d482880d4430
Varun-Patkar <varunpatkar501@gmail.com>
2021-04-03 12:31:48 +0530

    Did all database relations and models

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Post, Comment
+from .models import User, Post, Comment, Like_Tags, Dislike_Tags, Reply
 # Register your models here.
-from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Like_Tags)
+admin.site.register(Dislike_Tags)
+admin.site.register(Reply)
--- home/models.py
+++ home/models.py
@@ -1,44 +1,85 @@
 from djongo import models
+import uuid
 
 # Create your models here.
 
 
-class Tag(models.Model):
+class Like_Tags(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     tag = models.CharField(max_length=100)
 
-    class Meta:
-        abstract = True
+    def __str__(self):
+        return(self.tag)
+
+
+class Dislike_Tags(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return(self.tag)
 
 
 class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.CharField(max_length=200, null=True)
     username = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     securepassw = models.CharField(max_length=500)
-    # like_tags = models.ArrayField(Tag, null=True, blank=True)
-    # dislike_tags = models.ArrayField(Tag, null=True, blank=True)
-    # posts = multiple posts
-    # friends = other users
+    like_tags = models.ManyToManyField(Like_Tags)
+    dislike_tags = models.ManyToManyField(Dislike_Tags)
+    friends = models.ManyToManyField("self", symmetrical=True)
 
     def __str__(self):
         return(self.username)
 
 
 class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     createdTime = models.DateTimeField(auto_now_add=True)
     updatedTime = models.DateTimeField(auto_now=True)
-    # creator = user that created it
+    title = models.CharField(max_length=100, null=True)
+    content = models.CharField(max_length=500, null=True)
+    creator = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    # Comments(optional)
-    # image/s(optional)
-    # code(optional)
+    # image/s(optional)[One to Many]
+    # code(optional)[One to Many]
+
+    def __str__(self):
+        return(self.title)
 
 
 class Comment(models.Model):
-    # creator = user that created it
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4, editable=False)
+    creator = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True)
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
     createdTime = models.DateTimeField(auto_now_add=True)
     updatedTime = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=500, null=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    # Replys=array of comments(optional)
+
+    def __str__(self):
+        return(self.creator.full_name+" : "+self.content[:10])
+
+
+class Reply(models.Model):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4, editable=False)
+    parent_comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, null=True)
+    creator = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True)
+    createdTime = models.DateTimeField(auto_now_add=True)
+    updatedTime = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=500, null=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    replys = models.ManyToManyField("self", symmetrical=False)
+
+    def __str__(self):
+        return(self.creator.full_name+" : "+self.content[:10])
