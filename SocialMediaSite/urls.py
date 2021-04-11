acc300580888a96475c8e71f4a4d1084ca35d0ee
Varun-Patkar <varunpatkar501@gmail.com>
2021-04-11 19:01:03 +0530

    Added urlpaths for static files

--- SocialMediaSite/urls.py
+++ SocialMediaSite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home.views import view_404
+from django.conf import settings
+from django.conf.urls.static import static
 
 handler404 = view_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home.urls"))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
