84038994cd973408712616fd64664baf87957201
Varun-Patkar <varunpatkar501@gmail.com>
2021-05-17 11:00:53 +0530

    Removed unused models

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import User, Post, Comment, Like_Tags, Dislike_Tags, Reply
+from .models import User, Post, Comment, Images
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Like_Tags)
-admin.site.register(Dislike_Tags)
-admin.site.register(Reply)
+admin.site.register(Images)
--- home/models.py
+++ home/models.py
@@ -5,31 +5,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
-class Like_Tags(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    tag = models.CharField(max_length=100)
-
-    def __str__(self):
-        return(self.tag)
-
-
-class Dislike_Tags(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    tag = models.CharField(max_length=100)
-
-    def __str__(self):
-        return(self.tag)
-
-
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=200, unique=True)
     url = models.CharField(max_length=200, unique=True)
     email = models.CharField(max_length=100, null=True)
     password = models.CharField(max_length=500)
-    like_tags = models.ManyToManyField(Like_Tags)
-    dislike_tags = models.ManyToManyField(Dislike_Tags)
     friends = models.ManyToManyField("self", symmetrical=False)
     profile_picture = models.ImageField(upload_to="home/static/home/images/profile_pics/",
                               verbose_name='Image',null=True)
@@ -81,33 +62,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return(self.creator.full_name+" : "+self.content[:10])
-
-
-class Reply(models.Model):
-    id = models.UUIDField(primary_key=True,
-                          default=uuid.uuid4, editable=False)
-    parent_comment = models.ForeignKey(
-        Comment, on_delete=models.CASCADE, null=True)
-    creator = models.OneToOneField(
-        User, on_delete=models.CASCADE, null=True)
-    createdTime = models.DateTimeField(auto_now_add=True)
-    updatedTime = models.DateTimeField(auto_now=True)
-    content = models.CharField(max_length=500, null=True)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
-    replys = models.ManyToManyField("self", symmetrical=False)
-
-    def __str__(self):
-        return(self.creator.full_name+" : "+self.content[:10])
-
-# class Codes(models.Model):
-#     code_id = models.CharField()
-#     code_content = models.CharField(required=True)
-#     lang = models.CharField(required=True)
-#     code_input = models.CharField(required=True)
-#     compile_status= models.CharField(required=True)
-#     run_status_status=models.CharField(required=True)
-#     run_status_time=models.CharField(required=True)
-#     run_status_memory=models.CharField(required=True)
-#     run_status_output=models.CharField(required=True)
-#     run_status_stderr=models.CharField(required=True)
